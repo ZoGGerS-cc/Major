e9ab7e252818e605dabe2f1ed32442ea2da37aa8
ZoGGerS-cc <73020054+ZoGGerS-cc@users.noreply.github.com>
2021-04-12 13:53:12 +0500

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+//Block
+require("blocks/drill");
+require("blocks/conv");
+
+print("\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/");
+print("$$$$$$$$$$$$$$$$$$$");
+print("[#FFD700]Major Mod");
+print("[#FFD700]by ZoGGerS-Dim4ik");
+print("$$$$$$$$$$$$$$$$$$$");
+print("/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\");
+
+var mod = Vars.mods.locateMod("Major");
+mod.meta.displayName = "[#FFD700]Major";
+mod.meta.description = "@mod.major.description";
+mod.meta.author = "[#FFD700]ZoGGerS-Dim4ik";
