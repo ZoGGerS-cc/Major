c5c569a73870653cc588db560eee62b8022927f2
ZoGGerS-cc <73020054+ZoGGerS-cc@users.noreply.github.com>
2021-05-03 16:35:55 +0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,9 @@ require("blocks/drill");
 require("blocks/conv");
 require("blocks/mwall1");
 require("blocks/mwall2");
+require("blocks/lava-smelter");
+require("liquid/lava");
+
 
 print("[#FFD700]Major Mod");
 print("[#FFD700]by ZoGGerS-Dim4ik");
