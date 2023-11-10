209475b42c838cbc668b8dafd89bbb00bdbb9e48
ZoGGerS-cc <73020054+ZoGGerS-cc@users.noreply.github.com>
2023-11-11 01:17:50 +0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ require("blocks/mwall1");
 require("blocks/mwall2");
 
 
-print("[#AAF0D1]Major Mod");
+print("[#AAF0D1]relax_test_alpha");
 print("[#FFD700]by ZoGGerS");
 
 var mod = Vars.mods.locateMod("Major");
