184d293f2dd3b45547dea25c8a7602bfebe556b2
ZoGGerS-cc <73020054+ZoGGerS-cc@users.noreply.github.com>
2025-12-31 14:22:52 +0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,13 @@
+//Mod
+var mod = Vars.mods.locateMod("relax");
+mod.meta.displayName = "relax";
+mod.meta.description = "@mod.relax.description";
+mod.meta.author = "ZoGGerS & Dim4ik";
+
 //Block
 require("blocks/drill");
 require("blocks/conv");
 require("blocks/mwall1");
 require("blocks/mwall2");
 
-
-print("[#AAF0D1]relax_test_alpha");
 print("[#FFD700]by ZoGGerS & Dim4ik");
-
-var mod = Vars.mods.locateMod("relax");
-mod.meta.displayName = "relax";
-mod.meta.description = "@mod.relax.description";
-mod.meta.author = "ZoGGerS & Dim4ik";
